49de6cb3da730a6c65be9a2d3fff7d7bd963ee2d
Richard <therichard233@example.com>
2026-07-28 10:55:06 -0300

    primeiro commit: exercicios

---
+++ variaveis.js
@@ -0,0 +1,20 @@
+var nome = 'Richard'
+console.log(nome)
+
+let idade = '26 anos'
+console.log(idade)
+
+const cidade = 'Blumenau'
+console.log(cidade)
+
+let carro = {
+    nome: 'Golf GTI',
+    ano: '2020',
+    cores: [
+        'Preto',
+        'Branco',
+        'vermelho',
+    ]}
+
+console.log(carro)
+
