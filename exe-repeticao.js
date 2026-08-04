7d835cf3df3f21413de26f0fd5cff7517df037bd
Richard <therichard233@example.com>
2026-08-04 11:06:08 -0300

    Initial commit

--- exe-repeticao.js
+++ exe-repeticao.js
@@ -1,9 +1,24 @@
-const nomes = ['Eduardo', 'Maria', 'Fernando', 'Joao', 'Francisco']
+//const nomes = ['Eduardo', 'Maria', 'Fernando', 'Joao', 'Francisco']
 
-nomes.forEach((nome, indice) => {
-    console.log('execução:', indice +1)
-    console.log('nome'+ nome)
-    console.log('-------------------')
+//nomes.forEach((nome, indice) => {
+   // console.log('execução:', indice +1)
+   // console.log('nome:'+ nome)
+   // console.log('-------------------')
+//})
+
+//const nomes = ['Richard', 'Danieli', 'Andreia', 'Larisa', 'Alice'] 
+
+//nomes.forEach((nome, indice) => {
+//console.log('execução:', indice +1)
+//console.log('nome:' + nome)
+//console.log('-------------------')
+//})
+
+const cores = ['Branco', 'Azul', 'Verde', 'Preto', 'Roxo']
+
+cores.forEach ((cor, indice) => {
+   console.log('Execução de cor', indice +1)
+   console.log('cor:' + cor)
+   console.log ('______________')
 
 })
-
