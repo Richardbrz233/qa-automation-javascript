2b323eabb19fbb854cc3a1acd7f437070ecb46bf
Richard <therichard233@example.com>
2026-08-04 10:23:11 -0300

    atualizando

---
+++ exe-repeticao.js
@@ -0,0 +1,9 @@
+const nomes = ['Eduardo', 'Maria', 'Fernando', 'Joao', 'Francisco']
+
+nomes.forEach((nome, indice) => {
+    console.log('execução:', indice +1)
+    console.log('nome'+ nome)
+    console.log('-------------------')
+
+})
+
