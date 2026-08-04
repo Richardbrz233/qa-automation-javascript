0cde8ca28a58074c2206669c11ee81a8c3f6e994
Richard <therichard233@example.com>
2026-08-04 09:51:01 -0300

    atualizando

--- repeticao.js
+++ repeticao.js
@@ -1,6 +1,10 @@
 const cidades = ['sao paulo', 'rio de janeiro', 'blumenau', 'florianopolis']
 
-cidades.forEach((elemento, index) => {
-console.log(elemento)
-console.log('execução numero: ' +index)
-})
+//cidades.forEach((elemento, index) => {
+//console.log(elemento)
+//console.log('execução numero: ' +index)
+//})
+
+for (let indice = 0; indice < cidades.length; indice++) {
+console.log(cidades[indice])
+}
