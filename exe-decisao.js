fcd83d5760257687101b71912fc6bc207017a57b
Richard <therichard233@example.com>
2026-08-05 10:53:26 -0300

    atualizando

--- exe-decisao.js
+++ exe-decisao.js
@@ -1,6 +1,14 @@
-const cidaes = ['São Paulo', 'Rio de janeiro', 'Florianópolis', 'Recife']
+const cidades = ['São Paulo', 'Rio de janeiro', 'Florianópolis', 'Recife']
 
 cidades.forEach((cidade, indice) => {
     console.log('Execução: ', indice +1)
+    console.log('cidade: ', cidade)
 
+if (cidade == 'Florianópolis') {
+    console.log('Encontrado!')
+} else {
+    console.log('Não encontrado!')
+}
+
+console.log('------------------------')
 })
