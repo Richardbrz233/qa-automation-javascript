c30dae7c5ceacfe5b2f5b3d6995b02a38c6a7ab0
Richard <therichard233@example.com>
2026-08-07 10:49:04 -0300

    atualizando

---
+++ exe-funcoes.js
@@ -0,0 +1,8 @@
+function gerarNumero(numero1, numero2) {
+    const resultado = numero1 + numero2 
+    console.log('Numero1: ', numero1)
+    console.log('Numero2: ', numero2)
+    console.log('soma: ', resultado)
+}
+
+gerarNumero(12, 8)
---
+++ funcoes.js
@@ -0,0 +1,14 @@
+//function gerarNome(nome, sobrenome, idade) {
+  //  console.log('Nome: ', nome + sobrenome)
+  //  console.log('Idade: ', idade)
+//}
+
+function gerarNome(nome, sobrenome,) {
+    return('Nome: ' + nome + sobrenome)
+}
+
+//console.log(gerarNome('Luiz', ' da Silva', 18))
+//console.log(gerarNome('Richard', ' Correa', 25))
+
+const nomeGerado = gerarNome('Pedro', ' Carvalho')
+console.log(nomeGerado)
